5355989607567dd1ce79957564e5b9027e80ad42
Mary Griffus <mary.e.shultz@gmail.com>
2016-06-05 12:19:19 -0400

    test

--- js/landing/landing.controller.js
+++ js/landing/landing.controller.js
@@ -11,6 +11,24 @@
 
   function landingControllerFunc($location, stationFactory){
     var landingVm = this;
+    landingVm.getLines = function(station){
+      var colorDisp = {
+        "BL": "Blue",
+        "GR": "Green",
+        "OR": "Orange",
+        "RD": "Red",
+        "SV": "Silver",
+        "YL": "Yellow"
+      };
+      var lines = '';
+      for (var i = 0; i <=4; i++){
+        if (station[LineCode + i]){
+          console.log('here' + station[LineCode + i])
+          lines += ' ' + colorDisp[station[LineCode + i]]
+        }
+      }
+      return  '(' + lines + ' )';
+    }
 
     //populate the dropdown
     stationFactory
@@ -20,30 +38,11 @@
       })
 
       // add lines for stations with multiple tracksets
-      var getLines = function(station){
-        var colorDisp = {
-          "BL": "Blue",
-          "GR": "Green",
-          "OR": "Orange",
-          "RD": "Red",
-          "SV": "Silver",
-          "YL": "Yellow"
-        };
-        var lines = '';
-        for (var i = 0; i <=4; i++){
-          if (station[LineCode + i]){
-            console.log('here' + station[LineCode + i])
-            lines += ' ' + colorDisp[station[LineCode + i]]
-          }
-        }
-        return  '(' + lines + ' )';
-      }
-
       var names = [];
       for (var i = 0; i < landingVm.data.length; i++){
         if (landingVm.data[i].Name in names){
-          landingVm.data[i-1].Name += getLines(landingVm.data[i-1]);
-          landingVm.data[i].Name += getLines(landingVm.data[i]);
+          landingVm.data[i-1].Name += landingVm.getLines(landingVm.data[i-1]);
+          landingVm.data[i].Name += landingVm.getLines(landingVm.data[i]);
         }
         names.push(landingVm.data[i].Name);
       }
